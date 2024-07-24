01d079714f3132b282cf99643ef17fa2bb4a9fac
KelvinCodes <prive@kelvincodes.nl>
2024-07-24 09:10:47 +0200

    Added styling

--- html/form_html.php
+++ html/form_html.php
@@ -6,55 +6,55 @@
     // Firstname
 	echo '<p>';
 	echo '<strong>Voornaam</strong> (vereist) <br/>';
-	echo '<input type="text" name="firstname" size="40" required style="color: black !i;"/>';
+	echo '<input type="text" name="firstname" size="40" required style="color: black;"/>';
 	echo '</p>';
     
     // Last name
 	echo '<p>';
 	echo '<strong>Achternaam</strong> (vereist) <br/>';
-	echo '<input type="text" name="lastname" size="40" required/>';
+	echo '<input type="text" name="lastname" size="40" required style="color: black;"/>';
 	echo '</p>';
 
     // Address
 	echo '<p>';
 	echo '<strong>Adres</strong> (vereist) <br/>';
-	echo '<input type="text" name="address" size="40" required/>';
+	echo '<input type="text" name="address" size="40" required style="color: black;"/>';
 	echo '</p>';
     
     // City
 	echo '<p>';
 	echo '<strong>Woonplaats</strong> (vereist) <br/>';
-	echo '<input type="text" name="city" size="40" required/>';
+	echo '<input type="text" name="city" size="40" required style="color: black;"/>';
 	echo '</p>';
 	
     // Postcode
 	echo '<p>';
 	echo '<strong>Postcode</strong> (vereist) <br/>';
-	echo '<input type="text" name="postal_code" size="40" required/>';
+	echo '<input type="text" name="postal_code" size="40" required style="color: black;"/>';
 	echo '</p>';
 	
     // Email
 	echo '<p>';
 	echo '<strong>Email</strong> (vereist) <br/>';
-	echo '<input type="text" name="email" size="40" required/>';
+	echo '<input type="text" name="email" size="40" required style="color: black;"/>';
 	echo '</p>';	
 
     // Phonenumber
 	echo '<p>';
 	echo '<strong>Telefoonnummer</strong> (vereist) <br/>';
-	echo '<input type="text" name="phone" size="40" required/>';
+	echo '<input type="text" name="phone" size="40" required style="color: black;"/>';
 	echo '</p>';		
 
     // Birthdate
 	echo '<p>';
 	echo '<strong>Geboortedatum</strong> (DD-MM-YYYY) (vereist) <br/>';
-	echo '<input type="text" name="birthdate" size="40" required/>';
+	echo '<input type="text" name="birthdate" size="40" required style="color: black;"/>';
 	echo '</p>';	
 
     // Nationality
 	echo '<p>';
 	echo '<strong>Nationaliteit</strong> (vereist) <br/>';
-	echo '<input type="text" name="nationality" size="40" required/>';
+	echo '<input type="text" name="nationality" size="40" required style="color: black;"/>';
 	echo '</p>';		
 
     // Certificates
@@ -64,15 +64,15 @@
 
 	echo '<input type="hidden" value=0 name="glider_brevet">';
     echo '<input type="checkbox" id="glider_brevet" name="glider_brevet" value=1/>';
-    echo '<label for="glider_brevet" style="padding-left: 5px;"> Zweef brevet A</label><br>';
+    echo '<label for="glider_brevet" style="padding-left: 5px; color: black;"> Zweef brevet A</label><br>';
 
 	echo '<input type="hidden" value=0 name="motor_brevet">';
     echo '<input type="checkbox" id="motor_brevet" name="motor_brevet" value=1/>';
-    echo '<label for="motor_brevet" style="padding-left: 5px;"> Motor brevet A</label><br>';
+    echo '<label for="glider_brevet" style="padding-left: 5px; color: black;"> Motor brevet A</label><br>';
 
 	echo '<input type="hidden" value=0 name="heli_brevet">';
     echo '<input type="checkbox" id="heli_brevet" name="heli_brevet" value=1/>';
-    echo '<label for="heli_brevet" style="padding-left: 5px;"> Heli brevet A</label><br>';
+    echo '<label for="heli_brevet" style="padding-left: 5px; color: black;"> Heli brevet A</label><br>';
 
 	echo '<input type="hidden" value=0 name="drone_brevet_a1">';
     echo '<input type="checkbox" id="drone_brevet_a1" name="drone_brevet_a1" value=1/>';
@@ -80,16 +80,16 @@
 
 	echo '<input type="hidden" value=0 name="drone_brevet_a3">';
     echo '<input type="checkbox" id="drone_brevet_a3" name="drone_brevet_a3" value=1/>';
-    echo '<label for="drone_brevet_a3" style="padding-left: 5px;"> Drone EU A3</label><br>';
+    echo '<label for="drone_brevet_a3" style="padding-left: 5px; color: black;"> Drone EU A3</label><br>';
 
 	echo '<input type="hidden" value=0 name="drone_brevet_a2">';
     echo '<input type="checkbox" id="drone_brevet_a2" name="drone_brevet_a2" value=1/>';
-    echo '<label for="drone_brevet_a2" style="padding-left: 5px;"> Drone EU A2</label><br>';
+    echo '<label for="drone_brevet_a2" style="padding-left: 5px; color: black;"> Drone EU A2</label><br>';
 
     // RDW number
 	echo '<p>';
 	echo '<strong>RDW registratienummer</strong> <br/>';
-	echo '<input type="text" name="rdw_number" size="40"/>';
+	echo '<input type="text" name="rdw_number" size="40" style="color: black;"/>';
 	echo '</p>';		
 
     // Other club select
@@ -106,7 +106,7 @@
     // Other club if yes select
 	echo '<p>';
 	echo '<strong>Bij ja, welke modelvliegclub?</strong> <br/>';
-	echo '<input type="text" name="other_club_text" size="40"/>';
+	echo '<input type="text" name="other_club_text" size="40"/> style="color: black;"';
 	echo '</p>';		 
 
     // KNVvl select
@@ -123,13 +123,13 @@
     // If KNVvl is member, number
 	echo '<p>';
 	echo '<strong>Bij ja, K.N.V.v.l. registratienummer</strong> <br/>';
-	echo '<input type="text" name="knvvl_text" size="40"/>';
+	echo '<input type="text" name="knvvl_text" size="40" style="color: black;"/>';
 	echo '</p>';		 
 
     // Wanne be member of TRMC at:
 	echo '<p>';
 	echo '<strong>Ik wil lid worden van de T.R.M.C. per</strong> (DD-MM-YYYY) (vereist) <br/>';
-	echo '<input type="text" name="wanna_be_member_at" size="40" required/>';
+	echo '<input type="text" name="wanna_be_member_at" size="40" required style="color: black;"/>';
 	echo '</p>';	
 
     // Cringe spacing
